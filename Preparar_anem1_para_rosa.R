51a99804867e952edd31398d1f49a658d301f20e
eufrasio98 <andoni98@protonmail.com>
2018-10-30 13:08:47 +0100

    Mejorar el script Prepara... y que haga windrose bonito

--- Preparar_anem1_para_rosa.R
+++ Preparar_anem1_para_rosa.R
@@ -4,3 +4,12 @@ library(rlist)
 datos=list.load(here::here("data/Datos_anemometros.rdata"))
 anem1=datos$anem1
 anem1$Mean=as.numeric(as.character(anem1$Mean))
+
+nombres_nuevos=colnames(anem1)
+nombres_nuevos[3]="ws"
+nombres_nuevos[6]="wd"
+colnames(anem1)=nombres_nuevos
+
+windRose(anem1,int = 0.5,angle = 22.5,
+         breaks = 8,paddle = F, annotate = F,
+         key.position = "right", statistic = "prop.mean")
