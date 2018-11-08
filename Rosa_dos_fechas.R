2dc4f757f1a0ff14573c28c5e8723446b5fc2d6b
eufrasio98 <andoni98@protonmail.com>
2018-11-08 10:55:25 +0100

    Ir avanzando un poco en este script.

--- Rosa_dos_fechas.R
+++ Rosa_dos_fechas.R
@@ -1,31 +1,32 @@
-Rosa_dos_fechas=function(){
-  library(lubridate)
-  library(here)
-  library(rlist)
+library(lubridate)
+library(here)
+library(rlist)
+
+#Descargar datos ----
+datos=list.load(here::here("data/Datos_anemometros.rdata"))
   
-  #Descargar datos
-  datos=list.load(here::here("data/Datos_anemometros.rdata"))
-  
-  #Pedimos input de fechas,de entre que dos fechas se escogeran los datos?
-  dia_1=readline(prompt = "Dia?")
-  mes_1=readline(prompt = "Mes?")
-  año_1=readline(prompt = "Año?")
-  dia_2=readline(prompt = "Dia?")
-  mes_2=readline(prompt = "Mes?")
-  año_2=readline(prompt = "Año?")
-  
-  #Pedimos input de sensor, de  cual quiere hacer la rosa?
+#Pedir input de fechas y de sensor ----
+#De entre que dos fechas se escogeran los datos?
+dia_1=readline(prompt = "Dia?")
+mes_1=readline(prompt = "Mes?")
+año_1=readline(prompt = "Año?")
+dia_2=readline(prompt = "Dia?")
+mes_2=readline(prompt = "Mes?")
+año_2=readline(prompt = "Año?")
+
+#Pedimos input de sensor, de  cual quiere hacer la rosa?
+list.names(datos)
+sensor=readline("Cual de los sensores?")
+while(sum(list.names(datos)==sensor)==0){    #Seguir repitiendo hasta darle bien el sensor
   list.names(datos)
-  sensor=readline("Cual de los sensores?")
-  while(sum(lista_sensores==sensor)==0){    #Seguir repitiendo hasta darle bien el sensor
-    list.names(datos)
-    sensor=readline("Cual de los sensores? Escribelo exactamente igual")
-    
-  }
-  
-  #Ahora que tenemos el input y los datos vamos a escoger las lineas necesarias
-  x=which(list.names==sensor)   #Vamos a ver en la lista que numero es el sensor. Utilizaremos x para coger datos mas tarde
-  
-  
+  sensor=readline("Cual de los sensores? Escribelo exactamente igual")
   
 }
+
+#Ahora que tenemos el input y los datos vamos a escoger las lineas necesarias
+x=which(list.names==sensor)   #Vamos a ver en la lista que numero es el sensor. Utilizaremos x para coger datos mas tarde
+
+  
+#Borrar variables sobrantes ----
+remove(dia_1,dia_2,mes_1,mes_2,año_1,año_2)   #Borrar variables de fechas pedidas al usuario
+remove(sensor)  #Borrar otras variables
