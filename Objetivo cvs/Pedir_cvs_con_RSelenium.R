473616b4e6695d41f50950d234d757abbfe28cae
eufrasio98 <andoni98@protonmail.com>
2018-11-18 23:49:00 +0100

    Sigo haciendo el script de bajar cvs.

--- Objetivo cvs/Pedir_cvs_con_RSelenium.R
+++ Objetivo cvs/Pedir_cvs_con_RSelenium.R
@@ -9,4 +9,22 @@ navegador=a$client      #el objeto navegador es el que nos va a hacer todo el cu
 navegador$navigate(url)   #Nuestra venta de firefox se mete en el link k le hemos puesto en la variable url
 #Truco para ir usando el navegador: Si escribimos  navegador$ y tabulamos nos salen las posibles opciones (como el navigate de arriba). Las opciones vienen explicadas en el link que he `puesto arriba`
 
+#Buscamos las cajas del texto donde se escriben las fechas----
+fechainicio="11/08/2018 6:57 PM"
+fechafinal="11/15/2018 6:57 PM"   #Cuando el programa este acabado las fechas deberian ser input de la funcion actualizadora
+cajatexto_fechainicio=navegador$findElement(using = 'css selector', value = "#from")  #Encontar las cajetilla de la primera fecha
+cajatexto_fechafinal=navegador$findElement(using = 'css selector', value = "#to")     #Encontrar la de la segunda
+cajatexto_fechainicio$clearElement()  #Vaciar la primera cajetilla
+cajatexto_fechafinal$clearElement()  #Vaciar la segunda cajetilla
+cajatexto_fechainicio$sendKeysToElement(list(fechainicio))  #Escribir fechainicio en la primera cajetilla
+cajatexto_fechafinal$sendKeysToElement(list(fechafinal))    #Escribir fechafinal en la segunda cajetilla
+
+#Antes de darle al boton de exportar tenemos que cambiar unos ajustes de nuestra ventana de firefox
+#El problema es que si al exportar nos sale la mitica ventana de Abrir archivo/Guardar archivo
+#Selenium no puede leerlo por que es una ventana de windows, no de firefox
+#Cambiamos los ajustes para que se descargue directamente al darle al boton
+
+#Tambien buscamos el boton de exportar
+boton_exportar=navegador$findElement(using = 'css selector',value = "button.btn:nth-child(10)")
+boton_exportar$clickElement()
 
