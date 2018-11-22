8ccb1bbce0784780b92b36fd97d69c625e36cb66
eufrasio98 <andoni98@protonmail.com>
2018-11-22 13:24:08 +0100

    Abandonar lo de fprof y poner bonitos los datos de la primera pagina de mobile alerts.

--- Objetivo_cvs/Pedir_cvs_con_RSelenium.R
+++ Objetivo_cvs/Pedir_cvs_con_RSelenium.R
@@ -1,12 +1,14 @@
 library(RSelenium)
 library(here)
+library(stringr)
+library(lubridate)
 #Dejo link tutorial de este paquete:
 #https://www.computerworld.com/article/2971265/application-development/how-to-drive-a-web-browser-with-r-and-rselenium.html
 #Para ir probando pongo por ahora solo el link del Anem Hexagono. Cuando se vea que funciona habria k generalizarlo para poder usarlo en cualquier sensor
 url="https://measurements.mobile-alerts.eu/Home/MeasurementDetails?deviceid=0B75FE3A4FB6&vendorid=f193c634-2611-475b-ba7a-27b0ead33c6f&appbundle=eu.mobile_alerts.mobilealerts"
 #directorio_descarga=paste(here::here(),"/Objetivo_cvs",sep = "")
 #directorio_perfil=paste(here::here(),"/Objetivo_cvs/perfil",sep = "")
-fprof=makeFirefoxProfile(list(browser.download.dir = directorio_descarga))
+#fprof=makeFirefoxProfile(list(browser.download.dir = directorio_descarga))
 #browser.helperApps.neverAsk.saveToDisk = 'application/zip'
 rD=rsDriver(browser = "firefox")  #No se muy bien que hace pero es necesario para crear el navegador. Firefox mejor que chrome no?
 #,extraCapabilities = fprof
@@ -26,21 +28,45 @@ cajatexto_fechainicio$sendKeysToElement(list(fechainicio))  #Escribir fechainici
 cajatexto_fechafinal$sendKeysToElement(list(fechafinal))    #Escribir fechafinal en la segunda cajetilla
 
 #Tambien buscamos el boton de exportar
-boton_exportar=remDr$findElement(using = 'css selector',value = "button.btn:nth-child(10)")
-boton_exportar$clickElement()
+#boton_exportar=remDr$findElement(using = 'css selector',value = "button.btn:nth-child(10)")
+#boton_exportar$clickElement()
 
 #Tambien buscamos el boton de refrescar
-#boton_refrescar=remDr$findElement(using = 'css selector',value = "button.btn:nth-child(9)")
-#boton_refrescar$clickElement()
+boton_refrescar=remDr$findElement(using = 'css selector',value = "button.btn:nth-child(9)")
+boton_refrescar$clickElement()
+
+#De aqui para abajo pruebas que voy haciendo por si nos da por hacer Rselenium + webscrapping
+tabla=remDr$findElement(using = 'css selector', 
+                        value = ".table > tbody:nth-child(2)")$getElementText() #
+
+split_newline<- str_split(tabla[[1]],pattern = "\n")      #Separar tabla en filas
+split_newline_matrix<- t(rbind(unlist(split_newline)))    #Convertirlo en matriz
+lista_datos=str_split(split_newline_matrix,pattern = " ") #Pasarlo a lista separando por espacios
+matriz_datos=matrix(unlist(lista_datos),ncol = 8,byrow = TRUE)      #Mira la lista pot filas y lo mete en 8 columnas
+dataframe_datos=as.data.frame(matriz_datos)                         #Pasar a data.frame
+dataframe_datos[,1]=mdy_hms(paste(dataframe_datos[,1],dataframe_datos[,2],dataframe_datos[,3],sep = " "))  #Meter en la primera columna el dia y la hora en formato fecha             
+dataframe_datos=dataframe_datos[,c(1,4,6,8)]                #Coge solo las columnas que nos interesan (m/s fuera)
+dataframe_datos[,4]=as.character(dataframe_datos[,4])
+for (i in 1:length(dataframe_datos[,4])) {
+  yy<-dataframe_datos[,4][i]
+  dataframe_datos[i,4]<- ifelse(yy=="North", 0, ifelse(yy=="North-northeast",20,
+                                                   ifelse(yy=="Northeast",45, ifelse(yy=="East-northeast",65,
+                                                                                     ifelse(yy=="East", 90, 
+                                                                                            ifelse(yy=="East-southeast", 110,
+                                                                                                   ifelse(yy=="Southeast", 135,
+                                                                                                          ifelse(yy=="South-Southeast", 155, 
+                                                                                                                 ifelse(yy=="South", 180, 
+                                                                                                                        ifelse(yy=="South-southwest", 200, 
+                                                                                                                               ifelse(yy=="Southwest", 225,
+                                                                                                                                      ifelse(yy=="West-southwest", 245, 
+                                                                                                                                             ifelse(yy=="West", 270, 
+                                                                                                                                                    ifelse(yy=="West-northwest",290,
+                                                                                                                                                           ifelse(yy=="Northwest", 315,
+                                                                                                                                                                  ifelse(yy=="North-northwest", 335, 
+                                                                                                                                                                         ifelse(yy=='-31',NA)))))))))))))))))
+}
+colnames(dataframe_datos)=c("Date","Mean","Max","Dir_deg")   #Nombrar columnas
 
 #Cerrar el server y la ventana
 remDr$close()
 rD$server$stop()
-
-#De aqui para abajo pruebas que voy haciendo por si nos da por hacer Rselenium + webscrapping
-tabla=remDr$findElement(using = 'css selector', 
-                        value = ".table > tbody:nth-child(2)")$getElementText()
-
-split_newline<- str_split(tabla[[1]],pattern = "\n")
-split_newline_matrix<- t(rbind(unlist(split_newline)))
-str_split(split_newline_matrix[1,],pattern = " ")
