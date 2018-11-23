aa9c5c947ec8ec2d00fe2777519bc326350f27fe
oscargarciahernandez <garcia96.oscar@gmail.com>
2018-11-23 21:57:25 +0100

    Acabado el merge y elaborada la lista con todas líneas de todas las actualizaciones. [merged_dataset....]

--- Merge.R
+++ Merge.R
@@ -2,6 +2,7 @@ library(stringr)
 library(rlist)
 library(here)
 library(purrr)
+
 listas_datos<- list.files(here::here("data"))
 for (i in 1:length(listas_datos)) {
  assign(paste0("Datos_",i),list.load(paste0(here::here("data/"),"/",listas_datos[i])))
@@ -26,30 +27,37 @@ rbind(last_1,last_2,last_3)
 #El problema que encontré es que está la hora cambiada entre data-sets
 #el cambio horario parece que afecta a todo el registro 
 #valla putada, no entiendo porqué la página web ha hecho esto
+
 Date_new<- lapply(Datos_1,  function(x) if(is.data.frame(x)){x[,2]<-x[,2]-hm("01:00")}else{NA})
 for (i in 1:length(Date_new)) {
   if(is.data.frame(Datos_1[[i]])){
-    Datos_1[[i]][,2]<- Date_new[[i]]
+    Datos_1[[i]][,2]<- Date_new[[i]]}else{NA}}
+
+lista_datos<- list(Datos_1,Datos_2,Datos_3)
+range_datae<-lapply(lista_datos, function(x) sapply(x,function(y) if(is.data.frame(y)){range(y[,2])}else{NA}))
+
+
+lista_total<- list()
+for (i in 1:length(lista_datos[[1]])) {
+  if(is.data.frame(lista_datos[[1]][[i]])){
+    x<-lista_datos[[1]][[i]]
+    y<-lista_datos[[2]][[i]]
+    z<-rbind(x[!x[,2]%in%y[,2],],y)
     
-    
+    h<-lista_datos[[3]][[i]]
+    b<-rbind(z[!z[,2]%in%h[,2],],h)
+    final__dataframe<- b[order(b[,2]),]
+    lista_total[[i]]<- final__dataframe
   }else{NA}
-  
-}
 
-x<- Datos_1$anem1
-y<- Datos_2$anem1
-
-vector_cont<-vector()
-for (i in 1:length(x[,1])) {
-  vector_cont[i]<-is.element(x[i,1],y[,1])
-  
 }
 
 
-z<-which()
-z<-merge(x,y, by="s.since_1.1.1970")
+source(here::here('funciones_app_anemometros.R'))
+disp<-id_iden(640689911849)
 
-length(x[,1])
-length(y[,1])
-length(z[,1])
 
+names(lista_total)<- as.character(disp[,2])
+
+path_data <- here::here(paste0("data/Merged_dataset",Sys.Date(),".rdata"))
+list.save(lista_total,path_data)
