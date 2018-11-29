8d238200965d538801052183c9bb3a4dbf0aec91
agarmendia98 <garmenaitor1234@gmail.com>
2018-11-29 14:03:55 +0100

    Cambiar horario a UTC

---
+++ Cambio a UTC.R
@@ -0,0 +1,15 @@
+library(here)
+library(stringr)
+library(lubridate)
+library(rlist)
+datos<-list.load(here::here("data/Datos_Anemometros/Datos_anemometros.rdata"))
+cambio_to_UTC<-function(x){
+  a<- hour(with_tz(Sys.time(), tzone = Sys.timezone()))
+  b<- hour(with_tz(Sys.time(), tzone = "UTC"))
+  corregir_hora<- a-b
+  
+  x$Date<- x$Date-hm(paste(corregir_hora,":00"))
+  return(x)
+}
+
+a<-lapply(datos, cambio_to_UTC)
