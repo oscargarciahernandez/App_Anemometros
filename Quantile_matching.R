7ec494926e3c84bfb071e118310fb1e449804bb7
eufrasio98 <andoni98@protonmail.com>
2018-12-20 18:27:27 +0100

    Traducción al castellano de los #comentarios de Alain.

--- Quantile_matching.R
+++ Quantile_matching.R
@@ -1,11 +1,22 @@
 library(qmap)
 
-#transf funtzioa sortu, fobj 
+#transf funtzioa sortu, fobj
+#Crear la función de transferencia, fobj
+
 # kalibrazio oinarria, kal_oin
+# Base de calibracion, kal_oin
+
 # kalibratu beharrekoa, kal_behar
-# kalibratua, kal, periodo guztian  
+# Lo que se tiene que calibrar, kal_behar
+
+# kalibratua, kal, periodo guztian
+# Calibrado, kal, en todo el periodo
+
 # tarte, trans funtzioa sortzen den periodoa
-# guzti, kalibrazio periodo guztia 
+# tarte, el periodo que se crea en la funcion de transferencia
+
+# guzti, kalibrazio periodo guztia
+# guzti, todoel periodo de calibración
 
 
 fobj<-fitQmapQUANT(kal_oin, kal_behar[tarte] ) 
