68911d0779a38aeef5a6551927a5b64d13237138
oscargarciahernandez <garcia96.oscar@gmail.com>
2019-02-06 12:19:39 +0100

    Puliendo_multiprocees para descargar ERA5 data

--- NUEVO/Calibracion.R
+++ NUEVO/Calibracion.R
@@ -5,3 +5,14 @@ source(here::here("NUEVO/Libraries.R"))
 
 load(here::here("NUEVO/Data_anemometros/Anemometros.Rdata"))
 load(here::here("NUEVO/Data_ERA5/ERA5_df.Rdata"))  
+
+
+#Obtenemos puntos del ERA cercanos al Anemo
+pos_anem_uni<-c(43.179361, -2.488510)#lat,lon
+
+
+nearest_lat<- data_ERA_2018_ls$latitude[order(abs(data_ERA_2018_ls$latitude - pos_anem_uni[1]))[1:2]]
+nearest_lon<- data_ERA_2018_ls$longitude[order(abs(data_ERA_2018_ls$longitude - pos_anem_uni[2]))[1:2]]
+
+
+
