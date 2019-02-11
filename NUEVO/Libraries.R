bec94274d4265ecbe2e22ded8bd5e6f0d06aebb1
eufrasio98 <andoni98@protonmail.com>
2019-02-11 17:54:11 +0100

    Mover las funciones para rellenar huecos de mediciones de calibarcion.r a libraries.r. Se considera que estan terminadas.

--- NUEVO/Libraries.R
+++ NUEVO/Libraries.R
@@ -157,6 +157,58 @@ equal_dir_lab<-function(Tabla_CSV){
 
 # Funciones Calibración ---------------------------------------------------
 
+buscar_huecos_anemos=function(datos_anemos){
+  #Esta funcion busca que huecos tenemos en las mediciones.
+  #No sobreescribe nada, solo devuelve el dataframe huecos.
+  #huecos[1] muestra cual es la medicion posterior (en el tiempo) al hueco.
+  #huecos[2] muestra cual es la medicion anterior (en el tiempo) al hueco.
+  #Las fechas estan en numerico (segundos desde 1970-01-01)
+  huecos=data.frame(a=as.POSIXct(character(),tz="UTC"), b=as.POSIXct(character(),tz="UTC"))  #Creamos relleno de esta forma para que cada columna este ya en el formato que queremos
+  colnames(huecos)=c(colnames(datos_anemos)[1],"Date[i]-Date[i+1]")
+  cont=1
+  for(i in 1:(dim(datos_anemos)[1])-1){
+    diferencia=datos_anemos$Date[i]-datos_anemos$Date[i+1]  #En minutos
+    if (isTRUE(diferencia<=0)){
+      print(paste0("ERROR! datos_anemos$Date[",as.character(i-1),"]-datos_anemos$Date[",as.character(i),"]=",as.character(diferencia)," minutos"))
+    }
+    if (isTRUE(diferencia>7*1.5)){
+      huecos[cont,1]=datos_anemos$Date[i]
+      huecos[cont,2]=datos_anemos$Date[i+1]
+      cont=cont+1
+    }
+  }
+  return(huecos)
+}
+
+rellenar_huecos_anemos=function(datos_anemos){
+  #Esta funcion recoge un data.frame de formato estandar de anemos
+  #Devuelve un data.frame parecido, pero con las mediciones filtradas por MobileAlerts.
+  #Las lineas añadidas tienen la fecha en la que se supone que se tendrian que haber hecho mediciones.
+  #El resto (mean, gust, dir) son NAs.
+  huecos=buscar_huecos_anemos(datos_anemos)
+  lista=list()  #Vamos a crear un a lista que contega dataframes. Estos seran los cachitos que van a conformar datos_anemos_rellenado.
+  lista[[1]]=datos_anemos[1:which(datos_anemos$Date==huecos[1,1]),]
+  for (i in 1:(nrow(huecos)))
+    #Por cada hueco hay que crear un data.frame con los datos anteriores (anteriores en el data.frame, posteriores en el tiempo) y otro con las mediciones vacias (el relleno).
+  {#Primero el relleno, las lineas que parece que MobileAlerts nos ha filtrado, con NAs en vez de mediciones.
+    relleno=data.frame(a=as.POSIXct(character(),tz="UTC"), b=numeric(), c=numeric(), d=numeric())  #Creamos relleno de esta forma para que cada columna este ya en el formato que queremos
+    for (j in 1:(as.numeric(round((huecos[i,1]-huecos[i,2]))/7)-1)) #Cuantas mediciones faltan? Solo una si huecos[i,2] ~= 2*7*60 mins, 2 si huecos[i,2] ~= 3*7*60 ...
+    {relleno[j,1]=datos_anemos$Date[which(datos_anemos$Date==huecos[i,1])]-j*(huecos[i,1]-huecos[i,2])/(as.numeric(round((huecos[i,1]-huecos[i,2]))/7))
+    #relleno[j,1]=la fecha posterior en el tiempo al hueco - j*(tamaño del hueco)/(el numero de mediciones que faltan en este hueco)
+    }
+    colnames(relleno)=colnames(datos_anemos)
+    lista[[2*i]]=relleno
+    if (i==(nrow(huecos))) {
+      lista[[length(lista)+1]]=datos_anemos[which(datos_anemos$Date==huecos[i,2]):nrow(datos_anemos),] #Si estamos con el ultimo hueco, hay que coger todo hasta el final de datos_anemos
+    }else{
+      lista[[2*i+1]]=datos_anemos[which(datos_anemos$Date==huecos[i,2]):which(datos_anemos$Date==huecos[i+1,1]),] #Las mediciones entre el principio del hueco y el final del siguiente hueco
+    }
+  }
+  datos_anemos_rellenado=do.call("rbind", lista) #Juntar todos los cachitos. Haciendo esto en vez de monton de rbins usamos menos recursos del pc, y tarda mucho menos.
+  colnames(datos_anemos_rellenado)=colnames(datos_anemos)
+  return(datos_anemos_rellenado)
+}
+
 #!!!!!!. Filtrar_datos no esta del todo adaptado por que nuestros datos de anemos no tienen $dir en numerico, sino en character.
 filtrar_datos=function(datos_anemos){
   #Esta funcion trabaja con un data.frame de mediciones de anemos que sigan nuestro
