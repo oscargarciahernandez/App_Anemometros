8594a0e4af20461a4ee852bc8a37345f4773c238
eufrasio98 <andoni98@protonmail.com>
2019-03-03 23:00:54 +0100

    Poner puntos en el mapa de OpenStreetMap: conseguido!

--- NUEVO/mapas.r
+++ NUEVO/mapas.r
@@ -12,6 +12,7 @@ library(maps)
 library(mapdata)
 library(OpenStreetMap)
 library(rJava)
+library(rgdal)
 
 Coordenadas_anemos=data.frame(a=as.numeric(),b=as.numeric())
 colnames(Coordenadas_anemos)=c("lat","lon")
@@ -30,24 +31,40 @@ ul <- c(n,w)  #Upper Left
 lr <- c(s,e)  #Lower Right
 par(mfrow=c(2,3))
 url <- "https://a.tiles.mapbox.com/v4/mapquest.streets-mb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwcXVlc3QiLCJhIjoiY2Q2N2RlMmNhY2NiZTRkMzlmZjJmZDk0NWU0ZGJlNTMifQ.mPRiEubbajc6a5y9ISgydg"
+
 #nm contiene todos los ppsibles atributos de type del comando openmap
-#"osm" es el mapa estandar de OpenStreetMap, "stamen-terrain" es mas limpio y se ve la orografia muy bien, "bing" son fotos aereas, sin etiquetas.
 #https://www.r-bloggers.com/the-openstreetmap-package-opens-up/
-nm = c("osm", "maptoolkit-topo",
-       "waze", "mapquest", "mapquest-aerial",
+#Dejo solo en nm los que funcionan por el estado, y escribo aqui la descripcion de los tipos de mapa que podrian ser interesantes
+  #osm: mapa estandar OpenStreetMap
+  #mapquest: mapa topografico, soso pero preciso
+  #bing: fotos aereas sin etiquetas ni nada
+  #stamen-toner: mapas poco precisos en blanco y negro, ninguna utilidad cientifica, pero puede quedar chulo, muy industrial
+  #stamen-terrain: mapa con los relieves marcados con sombras, de un vistazo se ve bien la forma del terreno
+  #stamen-watercolor: tipo acuarela, solo valor artistico, mola si haces mucho zoom en una ciudad
+  #esri-topo: parecido a mapquest
+nm = c("osm", "mapquest",
        "bing", "stamen-toner", "stamen-terrain",
        "stamen-watercolor", "osm-german", "osm-wanderreitkarte",
-       "mapbox", "esri", "esri-topo",
-       "nps", "apple-iphoto", "skobbler",
+       "esri", "esri-topo",
        "opencyclemap", "osm-transport",
        "osm-public-transport", "osm-bbike", "osm-bbike-german")
-map <- openmap(ul,lr, minNumTiles=20,type="stamen-terrain",zoom=NULL)  #minNumTiles es proporcional a la resolucion del mapa. Muy pequeño: no se ve una mierda. Muy grande: da error
+map <- openmap(ul,lr, minNumTiles=2,type="apple-iphoto",zoom=NULL)  #minNumTiles es proporcional a la resolucion del mapa. Muy pequeño: no se ve una mierda. Muy grande: da error
 graphics.off()
 plot(map)
 #Esto abre una interfaz de Java que te permite navegar por el mapa del mundo
 #Sirve para ver como quedarian los mapas y para conseguir coordendas, nada mas
 launchMapHelper()
-#Intento de marcar puntos en el mapa
-points(x=0,y=0,col="blue",lwd=98)
-points(-2.5,43.15,lwd=1000000)
+#Marcar puntos anemos en rojo
+Coordenadas_anemos_spt=Coordenadas_anemos     #Vamos a crear un objeto espacial (_spt) sin sobrescribir el orginal (el dataframe)
+coordinates(Coordenadas_anemos_spt)<-~lon+lat
+proj4string(Coordenadas_anemos_spt)<-CRS("+init=epsg:4326")
+points(spTransform(Coordenadas_anemos_spt,osm()),lwd=5,col="red")
+#Marcar puntos era en morado
+Coordenadas_era_spt=Coordenadas_era     #Vamos a crear un objeto espacial (_spt) sin sobrescribir el orginal (el dataframe)
+coordinates(Coordenadas_era_spt)<-~lon+lat
+proj4string(Coordenadas_era_spt)<-CRS("+init=epsg:4326")
+points(spTransform(Coordenadas_era_spt,osm()),lwd=5,col="purple")
 
+#Si vemos que puntos tiene plot, nos hacemos una idea de como funciona esto
+axis(side = 1)
+axis(side = 2)
