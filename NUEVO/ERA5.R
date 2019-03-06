574f51b33a6a41b853afd6c87e059f4d2f270503
oscargarciahernandez <garcia96.oscar@gmail.com>
2019-03-06 16:20:41 +0100

    Avanzando con los mapas, fuera de la carpeta NUEVO... mapas2.R

--- NUEVO/ERA5.R
+++ NUEVO/ERA5.R
@@ -8,7 +8,6 @@ if(file.exists(here::here("NUEVO/Data_ERA5/ERA5_df.Rdata"))){}else{
 
 #Importar
 data_ERA_2018<- open.nc(here::here("NUEVO/Data_ERA5/Data_2018.nc"))
-#ARREGLO:cambiar "python/Data_ERA5/Data_2018.nc" por "python/Data_2018.nc"
 
 #Crear lista
 data_ERA_2018_ls<- read.nc(data_ERA_2018, unpack = TRUE)
