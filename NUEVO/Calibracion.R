88c351a621ce40e4be0d2bc1d5331e27975a4093
eufrasio98 <andoni98@protonmail.com>
2019-04-15 12:14:12 +0200

    Usar rds para datos_era y datos_juntos
    -Los datos de los n puntos de era y los de era + anemo (aka dAtos_juntos) pasan a guardarse en .rds
    -El antiguo datos_uni pasa a llamarse datos_juntos, para que el codigo sirva para cualquier proyecto
    -A la hora de establecer los puntos de era mas cercanos al anemo, coordenadas_anemos pasa a ser extraido de Tabla_registro.csv en funcion del valor de anemo_elegido

--- NUEVO/Calibracion.R
+++ NUEVO/Calibracion.R
@@ -6,14 +6,14 @@ source(here::here("NUEVO/Libraries.R"))
 load(here::here("NUEVO/Data_anemometros/Anemometros.Rdata"))
 load(here::here("NUEVO/Data_ERA5/ERA5_df.Rdata"))  
 
-#Datos_anemos:elegir anemo, crear, rellenar, filtrar, guardar----
+#Datos_anemos:elegir anemo, crear, rellenar, filtrar----
 
 #Que anemo usaremos? Elegir
 if (!exists("t_reg")) {
   t_reg<- read.csv(here::here("NUEVO/Data_anemometros/TABLA_REGISTRO.csv"), sep=";")
 }
 levels(t_reg$ID)  #Que anemos tenemos? Enseña las IDs
-#anemo_elegido="0B38DAE79059"
+#anemo_elegido=levels(t_reg$ID)[1]
 anemo_elegido="0B75FE3A4FB6"
 datos_anemos=rellenar_huecos_anemos(Anemometros[[which(names(Anemometros)==as.character(anemo_elegido))]])
 #huecos=buscar_huecos_anemos(Anemometros$`0B38DAE79059`)  #Para saber donde nos ha metido NAs la funcion rellenar_huecos_anemos
@@ -69,7 +69,7 @@ windRose(datos_anemos,ws = "Gust",wd="Dir",paddle = F,key.header = "Gust [m/s]")
 datos_anemos[N_errores,c(2,3,4)]=NA
 datos_anemos[N_na,c(2,3,4)]=NA    #Esto parece redundante pero viene bien asegurarse
 
-#Quitar los primeros datos de los anemos de la uni, que no sirven de nada
+#Quitar los primeros datos de los anemos, que no sirven de nada
 if (anemo_elegido=="0B38DAE79059") {#Uni
   datos_anemos=datos_anemos[-(1:which(as.character(datos_anemos$Date)=="2018-05-21 10:13:42")),]
 }
@@ -87,19 +87,17 @@ saveRDS(datos_anemos,
 #Cargar los resultados
 datos_anemos=readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_tratado.rds")))
 
-#Tratar datos era----
+#Tratar datos era, guardar, cargar----
 #Coordenadas que queremos representar
-
-Coordenadas_anemos=data.frame(a=as.numeric(),b=as.numeric())
-colnames(Coordenadas_anemos)=c("lon","lat")
-if (anemo_elegido=="0B38DAE79059") {
-  Coordenadas_anemos[1,]=c(-2.488504,43.179389)
+if (!exists("t_reg")) {#Cargamos la tabla de registro, para poder extraer coordenadas_anemos de anemo_elegido
+  t_reg<- read.csv(here::here("NUEVO/Data_anemometros/TABLA_REGISTRO.csv"), sep=";")
 }
+Coordenadas_anemos=filter(t_reg,ID==anemo_elegido) %>% select(lon,lat)  #Primero lon, luego lat
 
 #Pongo este if por que el comando unique tarda lo suyo, para evitar que se ejecute mas de lo necesario
 if (!exists("Coordenadas_era")) {
   Coordenadas_era=unique(ERA5_df[,c(2,3)])
-  Coordenadas_era=arrange(as_tibble(Coordenadas_era),lon,lat)  #Asegurarse orden correcto para distm
+  Coordenadas_era=arrange(Coordenadas_era,lon,lat)  #Asegurarse orden correcto para distm
 }
 #Ordenarlos de cercanos a lejanos
 #Para que distm funcione bien, en las columnas primero lon, luego lat, justo lo contrario de era
@@ -107,6 +105,7 @@ Coordenadas_era=Coordenadas_era[order(distm(Coordenadas_era, Coordenadas_anemos,
 #Coger los n mas cercanos
 n=9
 Coordenadas_era=Coordenadas_era[1:n,]
+rm(n)
 
 #De todo ERA5_df, coger solo los datos relativos a los puntos de Coordendas_era
 datos_era=filter(ERA5_df,lat %in% Coordenadas_era$lat,lon %in% Coordenadas_era$lon)
@@ -125,17 +124,17 @@ for (i in 2:nrow(Coordenadas_era)) {
 }
 #Vamos a dar el cambiazo
 datos_era=datos_era2
-rm(datos_era2)
+rm(datos_era2,Coordenadas_anemos,Coordenadas_era)
 
 #Guardar datos_era
 if(!dir.exists(here::here("NUEVO/Data_calibracion"))){
   dir.create(here::here("NUEVO/Data_calibracion"))
 }
-save(datos_era,
-     file=here::here("NUEVO/Data_calibracion/datos_era.Rdata"))
+saveRDS(datos_era,
+        file=here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_era.rds")))
 
 #Cargarlos
-load(here::here("NUEVO/Data_calibracion/datos_era.Rdata"))
+datos_era=readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_era.rds")))
 
 #Plotear datos_era----
 #Plotear de n en n
@@ -177,42 +176,41 @@ datos_era=ERA5_df[which((ERA5_df$lon==Coordenadas_era$lon)&(ERA5_df$lat==Coorden
 #Comparar anemos con era----
 
 #Para ejecutar esta seccion datos_era y datos_anemos deberian existir en el environment
-load(here::here("NUEVO/Data_calibracion/datos_era.Rdata"))
-load(here::here("NUEVO/Data_calibracion/datos_uni_tratados.Rdata"))
+datos_anemos=readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_tratado.rds")))
+datos_era=readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_era.rds")))
 
 #Le añadimos los datos del anemo a datos_era
-datos_uni=juntar_datos(datos_era,datos_anemos,nombres_col_fechas = c("Date1","Date"))
+datos_juntos=juntar_datos(datos_era,datos_anemos,nombres_col_fechas = c("Date1","Date"))
 #Queda mas guapo columnas de anemo a la izquierda y lo de era a la derecha no? (Modo chungo de reordenarlo, pero bueno)
-datos_uni=datos_uni[,c((ncol(datos_uni)+1-ncol(datos_anemos)):ncol(datos_uni),1:(ncol(datos_uni)-ncol(datos_anemos)))]
+datos_juntos=datos_juntos[,c((ncol(datos_juntos)+1-ncol(datos_anemos)):ncol(datos_juntos),1:(ncol(datos_juntos)-ncol(datos_anemos)))]
 
-
-#Guardar datos_uni
+#Guardar datos_juntos
 if(!dir.exists(here::here("NUEVO/Data_calibracion"))){dir.create(here::here("NUEVO/Data_calibracion"))}
-save(datos_uni,
-     file=here::here("NUEVO/Data_calibracion/datos_uni_hex.Rdata"))
+saveRDS(datos_juntos,
+     file=here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_juntos.rds")))
 
-
-load(here::here("NUEVO/Data_calibracion/datos_uni.Rdata"))
+#Cargar
+datos_juntos=readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_juntos.rds")))
 
 #Comparar anemo con todos los puntos
 cors_anemo_vs_puntos=c()
-col_mean=grep("Mean",colnames(datos_uni)) #Numeros de columna cuyo nombre contenga "Mean"
-for (i in grep("uv_wind",colnames(datos_uni))) { #Vector: Numeros de columnas cuyos nombres contengan "uv_wind
-  cors_anemo_vs_puntos[length(cors_anemo_vs_puntos)+1]=cor(datos_uni[,i],datos_uni[,col_mean],"na")
+col_mean=grep("Mean",colnames(datos_juntos)) #Numeros de columna cuyo nombre contenga "Mean"
+for (i in grep("uv_wind",colnames(datos_juntos))) { #Vector: Numeros de columnas cuyos nombres contengan "uv_wind
+  cors_anemo_vs_puntos[length(cors_anemo_vs_puntos)+1]=cor(datos_juntos[,i],datos_juntos[,col_mean],"na")
 }
 rm(col_mean)
 
 #Comparar anemo con todos los puntos por direcciones
 cors_anemo_vs_puntos=data.frame()
-col_mean=grep("Mean",colnames(datos_uni)) #Numeros de columna cuyo nombre contenga "Mean"
-dirs=unique(na.omit(datos_uni$Dir))
+col_mean=grep("Mean",colnames(datos_juntos)) #Numeros de columna cuyo nombre contenga "Mean"
+dirs=unique(na.omit(datos_juntos$Dir))
 dirs=dirs[order(as.numeric(dirs))]
 kont_i=1
 #Bucle para correlaciones
-for (i in grep("uv_wind",colnames(datos_uni))) { #Vector: Numeros de columnas cuyos nombres contengan "uv_wind
+for (i in grep("uv_wind",colnames(datos_juntos))) { #Vector: Numeros de columnas cuyos nombres contengan "uv_wind
   kont_j=1
   for (j in dirs) {
-    cors_anemo_vs_puntos[kont_j,kont_i]=cor(datos_uni[which(datos_uni$Dir==j),i],datos_uni[which(datos_uni$Dir==j),col_mean],"na")
+    cors_anemo_vs_puntos[kont_j,kont_i]=cor(datos_juntos[which(datos_juntos$Dir==j),i],datos_juntos[which(datos_juntos$Dir==j),col_mean],"na")
   kont_j=kont_j+1
   }
   kont_i=kont_i+1
@@ -220,7 +218,7 @@ for (i in grep("uv_wind",colnames(datos_uni))) { #Vector: Numeros de columnas cu
 #Bucle para porcentajes
 porcentajes_dir=c()
 for (i in dirs) {
-  porcentajes_dir[i]=nrow(filter(datos_uni,Dir==i))*100/nrow(datos_uni)  #Col 3=porcentaje de datos en esa direccion
+  porcentajes_dir[i]=nrow(filter(datos_juntos,Dir==i))*100/nrow(datos_juntos)  #Col 3=porcentaje de datos en esa direccion
 }
 cors_anemo_vs_puntos[,ncol(cors_anemo_vs_puntos)+1]=porcentajes_dir
 colnames(cors_anemo_vs_puntos)=c(1:(ncol(cors_anemo_vs_puntos)-1),"%")
@@ -228,35 +226,35 @@ row.names(cors_anemo_vs_puntos)=dirs
 rm(col_mean,kont_i,kont_j,porcentajes_dir)
 
 #Separar por direcciones de anemos (creo que esto queda obsoleto)
-datos_uni_dir=list()
-dirs=unique(datos_uni$Dir)      #Que direcciones tenemos en el anemo?
+datos_juntos_dir=list()
+dirs=unique(datos_juntos$Dir)      #Que direcciones tenemos en el anemo?
 dirs=dirs[-which(is.na(dirs))]  #Quitar NA
 dirs=as.numeric(dirs)           #ESTAN EN CHARACTER!  Cambiar a numerico para que se ordenen bien
 dirs=dirs[order(dirs)]          #Ordenar de menor a mayor
 for (i in 1:length(dirs)) {
-  datos_uni_dir[[i]]=datos_uni[which(datos_uni$Dir==dirs[i]),]
+  datos_juntos_dir[[i]]=datos_juntos[which(datos_juntos$Dir==dirs[i]),]
 }
-names(datos_uni_dir)=dirs       #Los elementos de la lista se llamaran como la direcion que les corresponde
+names(datos_juntos_dir)=dirs       #Los elementos de la lista se llamaran como la direcion que les corresponde
 
 #Correlaciones por direcciones (creo que esto queda obsoleto)
 cors_dir=data.frame()  #Aqui iran las correlaciones correspondientes a cada direccion
-cors_dir[1:length(datos_uni_dir),1]=names(datos_uni_dir)  #Col 1=las direcciones
-for (i in 1:length(datos_uni_dir)) {  
-  cors_dir[i,2]=cor(datos_uni_dir[[i]]$uv_wind,datos_uni_dir[[i]]$Mean,"na") #Col 2=las correlaciones
-  cors_dir[i,3]=nrow(datos_uni_dir[[i]])*100/sum(unlist(lapply(datos_uni_dir,nrow)))  #Col 3=porcentaje de datos en esa direccion
+cors_dir[1:length(datos_juntos_dir),1]=names(datos_juntos_dir)  #Col 1=las direcciones
+for (i in 1:length(datos_juntos_dir)) {  
+  cors_dir[i,2]=cor(datos_juntos_dir[[i]]$uv_wind,datos_juntos_dir[[i]]$Mean,"na") #Col 2=las correlaciones
+  cors_dir[i,3]=nrow(datos_juntos_dir[[i]])*100/sum(unlist(lapply(datos_juntos_dir,nrow)))  #Col 3=porcentaje de datos en esa direccion
 }
 colnames(cors_dir)=c("Dir","cor","%")
 
-windRose(mydata = datos_uni,ws="Mean",wd = "Dir",paddle = F)
-windRose(mydata = datos_uni,ws="uv_wind",wd="uv_dwi",paddle = F)
-windRose(mydata = datos_uni,ws="Mean",wd = "Dir",ws2="uv_wind",wd2="uv_dwi",paddle = F)
+windRose(mydata = datos_juntos,ws="Mean",wd = "Dir",paddle = F)
+windRose(mydata = datos_juntos,ws="uv_wind",wd="uv_dwi",paddle = F)
+windRose(mydata = datos_juntos,ws="Mean",wd = "Dir",ws2="uv_wind",wd2="uv_dwi",paddle = F)
 
 
 
 n1=500
-n2=nrow(datos_uni)
-plot(datos_uni$Date[n1:n2],datos_uni$Mean[n1:n2],type="l")
-lines(datos_uni$Date[n1:n2],datos_uni$uv_wind[n1:n2],col="grey")
+n2=nrow(datos_juntos)
+plot(datos_juntos$Date[n1:n2],datos_juntos$Mean[n1:n2],type="l")
+lines(datos_juntos$Date[n1:n2],datos_juntos$uv_wind[n1:n2],col="grey")
 
 #Calculo factor K diferencia de modulo entre punto ERA y anemo
 zo=3 #[m] Centers of cities with tall buildings - Manwell pag 46, tabla 2.2
@@ -265,16 +263,16 @@ zr=401 + 10 #[m] Altura era = altitud segun google earth + 10m
 k=log(z/zo)/log(zr/zo)  #k=U(z)/U(zr)
 
 
-plot(x=datos_uni$Date,y=datos_uni$Mean,col="red",type="l")
-lines(x=datos_uni$Date,y=k*datos_uni$uv_wind)
+plot(x=datos_juntos$Date,y=datos_juntos$Mean,col="red",type="l")
+lines(x=datos_juntos$Date,y=k*datos_juntos$uv_wind)
 
 layout(c(1,2))
-plot(x=datos_uni$Date[2000:2500],y=datos_uni$Mean[2000:2500],col="red",type="l")
-lines(x=datos_uni$Date[2000:2500],y=k*datos_uni$uv_wind[2000:2500])
-plot(x=datos_uni$Date[2000:2500],y=datos_uni$Dir[2000:2500])
+plot(x=datos_juntos$Date[2000:2500],y=datos_juntos$Mean[2000:2500],col="red",type="l")
+lines(x=datos_juntos$Date[2000:2500],y=k*datos_juntos$uv_wind[2000:2500])
+plot(x=datos_juntos$Date[2000:2500],y=datos_juntos$Dir[2000:2500])
 
-plot(x=datos_uni_dir$`247.5`$Date[200:500],y=datos_uni_dir$`247.5`$Mean[200:500],col="red",type="l")
-lines(x=datos_uni_dir$`247.5`$Date[200:500],y=datos_uni_dir$`247.5`$uv_wind[200:500],col="grey")
-lines(x=datos_uni_dir$`247.5`$Date[200:500],y=k*datos_uni_dir$`247.5`$uv_wind[200:500],col="black")
+plot(x=datos_juntos_dir$`247.5`$Date[200:500],y=datos_juntos_dir$`247.5`$Mean[200:500],col="red",type="l")
+lines(x=datos_juntos_dir$`247.5`$Date[200:500],y=datos_juntos_dir$`247.5`$uv_wind[200:500],col="grey")
+lines(x=datos_juntos_dir$`247.5`$Date[200:500],y=k*datos_juntos_dir$`247.5`$uv_wind[200:500],col="black")
 
-#cut(datos_uni_dir[[1]]$uv_dwi, breaks = c(0,seq(11.5,349.5,22.5),360), labels = c(as.numeric(names(datos_uni_dir)),0)) 
+#cut(datos_juntos_dir[[1]]$uv_dwi, breaks = c(0,seq(11.5,349.5,22.5),360), labels = c(as.numeric(names(datos_juntos_dir)),0)) 
