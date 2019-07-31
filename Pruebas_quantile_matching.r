0bfe6afb7658d43c2f044ad74a6aae7f180ce819
eufrasio98 <andoni98@protonmail.com>
2019-07-31 14:16:49 +0200

    Quantile mapping con cross_validation
    Subido codigo para calibrar cada punto de era presente en datos_juntos haciendo quantile mappping con cross-validation

--- Pruebas_quantile_matching.r
+++ Pruebas_quantile_matching.r
@@ -1,7 +1,8 @@
 library(qmap)
 library(forecast)
-anemo_elegido="0B38DAE79059"
-#Preparar los datos con los que trabajar (solo punto mas cercano de era)----
+library(magrittr)
+library(data.table)
+#Preparar los datos con los que trabajar----
 #Que anemo usaremos? Elegir
 if (!exists("t_reg")) {
   t_reg<- read.csv(here::here("NUEVO/Data_anemometros/TABLA_REGISTRO.csv"), sep=";")
@@ -10,184 +11,63 @@ levels(t_reg$ID)  #Que anemos tenemos? Enseña las IDs
 #anemo_elegido=levels(t_reg$ID)[1]
 anemo_elegido="0B38DAE79059"
 datos_juntos <- readRDS(here::here(paste0("NUEVO/Data_calibracion/",anemo_elegido,"_juntos.rds")))
-datos_juntos=datos_juntos[,1:9] #Por ahora cogemos solo velocidad del punto mas cercano de era
 
 #Aunque solo doQmap de problemas con NA (fitQmap los tolera) es mas util quitarlos desde el principio
-datos_juntos=datos_juntos[complete.cases(datos_juntos$Mean)&complete.cases(datos_juntos$uv_wind1),]
+datos_juntos=datos_juntos %>% .[complete.cases(.[,c('Mean',grep('uv_wind',colnames(.),value=T))]),]
 
+#Formato
 datos_juntos$Dir=as.numeric(datos_juntos$Dir)
+datos_juntos=as.data.table(datos_juntos)
+#if is.data.table(datos_juntos):
+  #NO FUNCIONA: datos_juntos[a:b, <<nombre_columna>>] = <<algo>>
+  #en su lugar: datos_juntos[a:b, <<nombre_columna>> := <<algo>>]
+  #NO FUNCIONA: nombre_columna='Mean'  datos_juntos[,nombre_columna := <<algo>>]
+  #en su lugar: nombre_columna='Mean'  datos_juntos[,get(nombre_columna) := <<algo>>]
 
-#PRIMER EJERCICIO. Calibrar todo con todo----
-ftrans=fitQmapQUANT(datos_juntos$Mean,datos_juntos$uv_wind1)
-calibrado=doQmapQUANT(datos_juntos$uv_wind1,ftrans)
+#Quitar columnas innecesarias
+datos_juntos[,grep('(Gust)|(Dir)|(lon)|(lat)|(uv_dwi)|(Date\\d)',colnames(datos_juntos),value=T)]=NULL
 
-#Hora de analizar los resultados
-summary(datos_juntos$Mean)
-summary(calibrado)
-#Vemos que los quantiles, el max y el min encajan, pero no la media
-cor(calibrado,datos_juntos$Mean)
-cor(datos_juntos$uv_wind1,datos_juntos$Mean)
-cor(calibrado,datos_juntos$uv_wind1)
-
-#Mean absolute error=mean(abs(error)) [m/s]
-mean(abs(datos_juntos$Mean-datos_juntos$uv_wind1))
-mean(abs(datos_juntos$Mean-calibrado))
-
-#Root Mean Squared Error=sqrt(mean(error^2)) [m/s]
-sqrt(mean((datos_juntos$Mean-datos_juntos$uv_wind1)^2))
-sqrt(mean((datos_juntos$Mean-calibrado)^2))
-
-
-plot_n_graficos(x=datos_juntos$Date1,n=14,datos_juntos$Mean,calibrado,datos_juntos$uv_wind1,leyenda = c("Mean","Cal","ERA"),main = "Ej 1")
-
-#Vemos que el calibrado se tiene valores parecidos a los del anemo, pero progresa como ERA.
-
-rm(ftrans,calibrado)
-
-#SEGUNDO EJERCICIO. Dividir datos por la mitad e intentar calibrar una mitad con la otra----
-
-#Solo columnas 5 (Date1=fecha de era), 2 (Mean=medicion) y 8 (uv_wind1=modelado/era)
-mitad_1=datos_juntos[1:(nrow(datos_juntos)/2),c(5,2,8)]
-mitad_2=datos_juntos[-(1:(nrow(datos_juntos)/2)),c(5,2,8)]
-
-#Crear funciones de transferencia (ftrans). Permite NAs.
-ftrans_mitad_1<-fitQmapQUANT(mitad_1$Mean,mitad_1$uv_wind1)
-ftrans_mitad_2<-fitQmapQUANT(mitad_2$Mean,mitad_2$uv_wind1)
-
-#Deducir viento. Cada mitad se calibra con la ftrans sacada de la otra mitad.
-calibrado_mitad_1 <- doQmapQUANT(mitad_1$uv_wind1,ftrans_mitad_2)
-calibrado_mitad_2 <- doQmapQUANT(mitad_2$uv_wind1,ftrans_mitad_1)
-
-#Hora de analizar los resultados mitad_1
-cor(calibrado_mitad_1,mitad_1$Mean)
-cor(mitad_1$Mean,mitad_1$uv_wind1)
-cor(calibrado_mitad_1,mitad_1$uv_wind1)
-
-#Mean absolute error=mean(abs(error)) [m/s]
-mean(abs(mitad_1$Mean-mitad_1$uv_wind1))
-mean(abs(mitad_1$Mean-calibrado_mitad_1))
-
-#Root Mean Squared Error=sqrt(mean(error^2)) [m/s]
-sqrt(mean((mitad_1$Mean-mitad_1$uv_wind1)^2))
-sqrt(mean((mitad_1$Mean-calibrado_mitad_1)^2))
-
-
-plot_n_graficos(x=mitad_1$Date1,n=7,mitad_1$Mean,calibrado_mitad_1,mitad_1$uv_wind1,leyenda = c("Mean","Cal","ERA"),main = "Ej 2 mitad_1")
-
-#Hora de analizar los resultados mitad_2
-cor(calibrado_mitad_2,mitad_2$Mean)
-cor(mitad_2$Mean,mitad_2$uv_wind1)
-cor(calibrado_mitad_2,mitad_2$uv_wind1)
-
-#Mean absolute error=mean(abs(error)) [m/s]
-mean(abs(mitad_2$Mean-mitad_2$uv_wind1))
-mean(abs(mitad_2$Mean-calibrado_mitad_2))
-
-#Root Mean Squared Error=sqrt(mean(error^2)) [m/s]
-sqrt(mean((mitad_2$Mean-mitad_1$uv_wind1)^2))
-sqrt(mean((mitad_2$Mean-calibrado_mitad_2)^2))
-
-
-plot_n_graficos(x=mitad_2$Date1,n=7,mitad_2$Mean,calibrado_mitad_2,mitad_2$uv_wind1,leyenda = c("Mean","Cal","ERA"),main = "Ej 2 mitad_2")
-
-remove(mitad_1,mitad_2,calibrado_mitad_1,calibrado_mitad_2,ftrans_mitad_1,ftrans_mitad_2)
-
-#TERCER EJERCICIO. Dividir datos por la mitad y hacer una calibracion para cada direccion de era.----
-
-#Columnas 5 (Date1=fecha de era), 2 (Mean=medicion), 8 (uv_wind1=modelado/era) y 9 (uv_dwi1=direccion era)
-mitad_1=datos_juntos[1:(nrow(datos_juntos)/2),c(5,2,8,9)]
-mitad_2=datos_juntos[-(1:(nrow(datos_juntos)/2)),c(5,2,8,9)]
-
-#Vamos a clasificar los datos de esta manera: mirar direccion de era y clasificar en 16 direcciones (como los anemos)
-direcciones=seq(0,360,360/16) #Aunque la direccion 0º y 360º sean la misma, por ahora los tratamos ppor separado para facilitar la separacion de las mediciones por direcciones. Luego las juntamos.
-mitad_1_por_dirs=list()
-mitad_2_por_dirs=list()
-
-for (i in 1:(length(direcciones)-1)) {  #-1 porque no queremos crear un elemento para 360º
-  mitad_1_por_dirs[[i]]=data.frame()
-  mitad_2_por_dirs[[i]]=data.frame()
+#Preparar cross_validation----
+nPartes=10  #Numero de partes en las que se va a dividir el histórico
+(range(datos_juntos$Date)[2]-range(datos_juntos$Date)[1])/nPartes #Cuanto tiempo tendra cada parte?
+for (i in 1:nPartes) {
+  datos_juntos[round((i-1)*nrow(datos_juntos)/nPartes):round(i*nrow(datos_juntos)/nPartes),Parte := i]
 }
-for (i in 1:nrow(mitad_1)) { #Bucle para clasificar lineas de mitad_1 por direcciones
-  (direcciones-mitad_1$uv_dwi1[i]) %>% abs %>% which.min -> x
-  if (x==17){x=1} #Si la direccion mas cercana es 360º, a partir de ahora lo contamos como 0º
-  #El codigo quedaria mas bonito con rbind, pero esto es mas eficiente (no hay que sobreescribir todo el rato, solo añadir lineas)
-  if (length(mitad_1_por_dirs[[x]])==0) {  #Si es la primera linea de la direccion x...
-    mitad_1_por_dirs[[x]]=mitad_1[i,] #...ponemos la linea tal cual.
-  }else{  #Si no es la primera linea de la direccion x, la añadimos debajo.
-    mitad_1_por_dirs[[x]][nrow(mitad_1_por_dirs[[x]])+1,1:ncol(mitad_1)]=mitad_1[i,]
+
+#1. Calibrar cada punto de era con cross-validation----
+#Crear funciones de transferencia (ftrans). #La de cada parte se entrena con las mediciones que NO son de la parte (Parte != i)
+lista_ftrans=list() #Lista de listas.
+for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  lista_ftrans[[i]]=list()
+  for (j in 1:nPartes) {
+    ftrans=fitQmapQUANT(datos_juntos[Parte != j,Mean],datos_juntos[Parte != j,get(col_era)])
+    #lista_ftrans=append(lista_ftrans,values = list(ftrans_i))  esto solo sirve si las columnas uv_wind estan ordenadas
+    lista_ftrans[[i]]=append(lista_ftrans[[i]],values = list(ftrans))
+  }
+
+}
+
+#Calibrar.
+for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  for (j in 1:nPartes) {
+    datos_juntos[Parte == j,paste0('qm',i) := doQmapQUANT(datos_juntos[Parte == j,get(col_era)],lista_ftrans[[i]][[j]])]
   }
 }
-for (i in 1:nrow(mitad_2)) { #Bucle para clasificar lineas de mitad_2 por direcciones
-  (direcciones-mitad_2$uv_dwi1[i]) %>% abs %>% which.min -> x
-  if (x==17){x=1} #Si la direccion mas cercana es 360º, a partir de ahora lo contamos como 0º
-  #El codigo quedaria mas bonito con rbind, pero esto es mas eficiente (no hay que sobreescribir todo el rato, solo añadir lineas)
-  if (length(mitad_2_por_dirs[[x]])==0) {  #Si es la primera linea de la direccion x...
-    mitad_2_por_dirs[[x]]=mitad_2[i,] #...ponemos la linea tal cual.
-  }else{  #Si no es la primera linea de la direccion x, la añadimos debajo.
-    mitad_2_por_dirs[[x]][nrow(mitad_2_por_dirs[[x]])+1,1:ncol(mitad_2)]=mitad_2[i,]
-  }
-}
-direcciones=direcciones[1:(length(direcciones)-1)] #Ya no nos hace falta el 360º, solo molesta.
-names(mitad_1_por_dirs)=direcciones
-names(mitad_2_por_dirs)=direcciones
 
-#Crear funciones de transferencia para cada direccion y para cada mitad
-lista_ftrans_mitad_1=list()
-lista_ftrans_mitad_2=list()
-for (i in 1:length(direcciones)) {
-  lista_ftrans_mitad_1[[i]]=fitQmapQUANT(mitad_1_por_dirs[[i]]$Mean,mitad_1_por_dirs[[i]]$uv_wind1)
-  lista_ftrans_mitad_2[[i]]=fitQmapQUANT(mitad_2_por_dirs[[i]]$Mean,mitad_2_por_dirs[[i]]$uv_wind1)
-}
-names(lista_ftrans_mitad_1)=direcciones
-names(lista_ftrans_mitad_2)=direcciones
-
-#Sacar datos calibrados para cada direccion y para cada mitad
-for (i in 1:length(direcciones)) {
-  mitad_1_por_dirs[[i]]=doQmapQUANT(mitad_1_por_dirs[[i]]$uv_wind1,lista_ftrans_mitad_2[[i]]) %>% cbind(mitad_1_por_dirs[[i]])
-  mitad_2_por_dirs[[i]]=doQmapQUANT(mitad_2_por_dirs[[i]]$uv_wind1,lista_ftrans_mitad_1[[i]]) %>% cbind(mitad_2_por_dirs[[i]])
-  colnames(mitad_1_por_dirs[[i]])[1]="Cal"
-  colnames(mitad_2_por_dirs[[i]])[1]="Cal"
+#Graficar
+for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos$uv_wind1,datos_juntos$qm1,leyenda = c("Mean","ERA","Cal"),main = "Punto 1")
 }
 
-#Reconstruir los datos calibrados de cada mitad como una serie temporal
-mitad_1=do.call(rbind,mitad_1_por_dirs) #Juntar las lineas de todas la direcciones en un solo dataframe
-mitad_2=do.call(rbind,mitad_1_por_dirs)
-mitad_1=mitad_1[order(mitad_1$Date1),]  #Ordenar segun fecha
-mitad_2=mitad_2[order(mitad_2$Date1),]
-mitad_1=mitad_1[c("Date1","Mean","Cal","uv_wind1","uv_dwi1")] #Reordenar columnas
-mitad_2=mitad_2[c("Date1","Mean","Cal","uv_wind1","uv_dwi1")]
-
-#Hora de analizar los resultados mitad_1
-cor(mitad_1$Cal,mitad_1$Mean)
-cor(mitad_1$Mean,mitad_1$uv_wind1)
-cor(mitad_1$Cal,mitad_1$uv_wind1)
-
-#Mean absolute error=mean(abs(error)) [m/s]
-mean(abs(mitad_1$Mean-mitad_1$uv_wind1))
-mean(abs(mitad_1$Mean-mitad_1$Cal))
-
-#Root Mean Squared Error=sqrt(mean(error^2)) [m/s]
-sqrt(mean((mitad_1$Mean-mitad_1$uv_wind1)^2))
-sqrt(mean((mitad_1$Mean-mitad_1$Cal)^2))
-
-
-plot_n_graficos(x=mitad_1$Date1,n=7,mitad_1$Mean,mitad_1$Cal,mitad_2$uv_wind1,leyenda = c("Mean","Cal","ERA"),main = "Ej 3 mitad_1")
-
-#Hora de analizar los resultados mitad_2
-cor(mitad_2$Cal,mitad_2$Mean)
-cor(mitad_2$Mean,mitad_2$uv_wind1)
-cor(mitad_2$Cal,mitad_2$uv_wind1)
-
-#Mean absolute error=mean(abs(error)) [m/s]
-mean(abs(mitad_2$Mean-mitad_2$uv_wind1))
-mean(abs(mitad_2$Mean-mitad_2$Cal))
-
-#Root Mean Squared Error=sqrt(mean(error^2)) [m/s]
-sqrt(mean((mitad_2$Mean-mitad_2$uv_wind1)^2))
-sqrt(mean((mitad_2$Mean-mitad_2$Cal)^2))
-
-
-plot_n_graficos(x=mitad_2$Date1,n=7,mitad_2$Mean,mitad_2$Cal,mitad_2$uv_wind1,leyenda = c("Mean","Cal","ERA"),main = "Ej 3 mitad_2")
-
-
+#Medidas
+for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  print(paste0('Medidas punto ',i))
+  print(accuracy(datos_juntos$Mean,datos_juntos[,get(col_era)]))
+  print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('qm',i))]))
+  print('----------------------')
+}
 
