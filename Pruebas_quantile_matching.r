7672fa9f4630ac485320d0693f8dde18fdb188fa
eufrasio98 <andoni98@protonmail.com>
2019-09-12 12:08:44 +0200

    Arreglos para homogeneizar todas los fors
    En Pruebas_quantile_matching.r se utilizaban muchos fors para modelar y calibrar punto por punto, pero para iteración se hacia de forma diferente (en una mirando que columnas de era habia en datos_juntos, en otra mirando las columnas de datos calibrados etc) y eso hacia que el código no fuese consistente. Ahora se hace todo mirando las columnas de era

--- Pruebas_quantile_matching.r
+++ Pruebas_quantile_matching.r
@@ -2,6 +2,11 @@ library(qmap)
 library(forecast)
 library(magrittr)
 library(data.table)
+library(plotrix)
+library(here)
+source(here::here('/NUEVO/Libraries.R'), encoding = 'UTF-8', echo=F)
+
+
 #Preparar los datos con los que trabajar----
 #Que anemo usaremos? Elegir
 if (!exists("t_reg")) {
@@ -27,6 +32,9 @@ datos_juntos=as.data.table(datos_juntos)
 #Quitar columnas innecesarias
 datos_juntos[,grep('(Gust)|(Dir)|(lon)|(lat)|(Date\\d)',colnames(datos_juntos),value=T)]=NULL
 
+#Detectar que columnas de era tenemos
+cols_era=grep('uv_wind\\d+',colnames(datos_juntos),value = T)
+
 #Preparar cross_validation (leave one out, mejor entrenamiento posible)----
 
 #En vez de ejecutar todo podemos cargar los resultados
@@ -45,7 +53,7 @@ for (i in 1:nPartes) {
 
 #Crear funciones de transferencia (ftrans). #La de cada parte se entrena con las mediciones que NO son de la parte (Parte != i)
 lista_ftrans=list() #Lista de listas.
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   lista_ftrans[[i]]=list()
   for (j in 1:nPartes) {
@@ -57,7 +65,7 @@ for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
 }
 
 #Calibrar.
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   for (j in 1:nPartes) {
     datos_juntos[Parte == j,paste0('qm',i) := doQmapQUANT(datos_juntos[Parte == j,get(col_era)],lista_ftrans[[i]][[j]])]
@@ -72,23 +80,31 @@ if (nPartes==nrow(datos_juntos)) {
 #Graficar punto por punto
 cols_qm_dirs=grep('qm\\d+_dirs',colnames(datos_juntos),value=T)
 cols_qm=grep('qm\\d',colnames(datos_juntos),value=T) %>% .[!(. %in% cols_qm_dirs)]
-for (col_qm in cols_qm) {
-  i=regmatches(col_qm, regexpr("\\d+", col_qm))  #numero del punto de era
-  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(paste0('uv_wind',i))],datos_juntos[,get(col_qm)],leyenda = c("Mean",paste0("ERA",i),col_qm),main = paste0('Punto ',i))
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(col_era)],datos_juntos[,get(paste0('qm',i))],leyenda = c("Mean",paste0("ERA",i),paste0('qm',i)),main = paste0('Punto ',i))
 }
 
 #Graficar todos los puntos, pero solo el calibrado y el anemo (codigo hecho para 9 puntos)
+#IDEA. Estaria bien mejorar plot_n_graficos para que pueda aceptar dataframes y/o listas en vez de un vector por argumento de ... Asi se podria hacer funcionar para un numero indeterminado de vectores a graficar
 plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,
                 datos_juntos$qm1,datos_juntos$qm2,datos_juntos$qm3,datos_juntos$qm4,datos_juntos$qm5,datos_juntos$qm6,datos_juntos$qm7,datos_juntos$qm8,datos_juntos$qm9,leyenda = c("Mean","qm1","qm2","qm3","qm4","qm5","qm6","qm7","qm8","qm9"),col = 'black')
 
 #Medidas
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   print(paste0('Punto ',i,' a pelo vs anemo'))
   print(accuracy(datos_juntos$Mean,datos_juntos[,get(col_era)]))
   print(paste0('Punto ',i,' calibrado vs anemo'))
   print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('qm',i))]))
   print('----------------------')
+}
+
+#Taylor
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(col_era)],add = F,main = paste0('Punto ',i,' - calibrado sin dirs'),col = 'red')
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(paste0('qm',i))],add = T,col = 'green')
 }
 
 
@@ -99,8 +115,8 @@ lista_ftrans_dirs=list() #Lista de listas de listas (toma ya!)
 #IDEA. Se podria hacer una matriz 2D de listas (por que los ftras son listas). Ayudaría en algo?
 
 #Por ahora separamos en 4 direcciones y a ver que pasa
-direcciones_era = 8 %>% seq(0,by=360/.,length.out = .)
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+direcciones_era = 32 %>% seq(0,by=360/.,length.out = .)
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   lista_ftrans[[i]]=list()
   for (dir in direcciones_era) {
@@ -115,7 +131,7 @@ for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
 }
 
 #Calibrar.
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   lista_ftrans[[i]]=list()
   for (dir in direcciones_era) {
@@ -124,23 +140,29 @@ for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
       ftrans=lista_ftrans_dirs[[i]][[as.character(dir)]][[nPartes]]
       #En vez de usar datatable, es necesario dar este rodeo a la hora cde calibrar por que con datatables vacios (de cuando no se cumplen las condicones) se lia
       posiciones_lineas=which((datos_juntos$Parte==j) & (((datos_juntos[,get(paste0('uv_dwi',i))] %% 360)-as.numeric(dir))<90.0000001))
-      datos_juntos[posiciones_lineas,(paste0('qm',i,'_dirs')) := doQmapQUANT(datos_juntos[posiciones_lineas,get(paste0('uv_wind',i))],ftrans)]
+      datos_juntos[posiciones_lineas,(paste0('qm',i,'_dirs')) := doQmapQUANT(datos_juntos[posiciones_lineas,get(col_era)],ftrans)]
     }
   }
 }
 
+#Vamos a guardar esto por que con lo que tarda en hacerse...
+if (nPartes==nrow(datos_juntos)) {
+  saveRDS(datos_juntos,here::here('/NUEVO/Data_calibracion/datos_juntos_leave_one_out.rds'))
+}
+
 #Graficar punto por punto
-for (col_qm_dirs in grep('qm\\d+_dirs',colnames(datos_juntos),value=T)) {
-  i=regmatches(col_qm_dirs, regexpr("\\d+", col_qm_dirs))  #numero del punto de era
-  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(paste0('uv_wind',i))],datos_juntos[,get(col_qm_dirs)],leyenda = c("Mean",paste0("ERA",i),col_qm_dirs),main = paste0('Punto ',i))
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(col_era)],datos_juntos[,get(paste0('qm',i,'_dirs'))],leyenda = c("Mean",paste0("ERA",i),paste0('qm',i,'_dirs')),main = paste0('Punto ',i))
 }
 
 #Graficar todos los puntos, pero solo el calibrado y el anemo (codigo hecho para 9 puntos)
+#IDEA. Estaria bien mejorar plot_n_graficos para que pueda aceptar dataframes y/o listas en vez de un vector por argumento de ... Asi se podria hacer funcionar para un numero indeterminado de vectores a graficar
 plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,
                 datos_juntos$qm1,datos_juntos$qm2,datos_juntos$qm3,datos_juntos$qm4,datos_juntos$qm5,datos_juntos$qm6,datos_juntos$qm7,datos_juntos$qm8,datos_juntos$qm9,leyenda = c("Mean","qm1","qm2","qm3","qm4","qm5","qm6","qm7","qm8","qm9"),col = 'black')
 
 #Medidas
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
+for (col_era in cols_era) {
   i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   print(paste0('Punto ',i,' a pelo vs anemo'))
   print(accuracy(datos_juntos$Mean,datos_juntos[,get(col_era)]))
@@ -149,24 +171,28 @@ for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
   print('----------------------')
 }
 
-#Calibrado sin dirs vs por dirs (1 vs 2)----
-
-cols_qm_dirs=grep('qm\\d+_dirs',colnames(datos_juntos),value=T)
-cols_qm=grep('qm\\d',colnames(datos_juntos),value=T) %>% .[!(. %in% cols_qm_dirs)]
-
-#Graficar punto por punto
-#Si los puntos que de era que tienen columna 'qm\\d+' no son los mismos que tienen 'qm_\\d+_dirs'...
-if(any((cols_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist %>% order)!=(cols_qm_dirs %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist %>% order))) {
-  print('ERROR. Parece que no has calibrado los mismos puntos sin dirs que con dirs. Estas seguro de que has hecho las dos calibraciones?')
-}else{
-  for (j in 1:length(cols_qm)) {
-    i=regmatches(cols_qm[j], regexpr("\\d+", cols_qm[j]))  #numero del punto de era
-    plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(paste0('uv_wind',i))],datos_juntos[,get(cols_qm[j])],datos_juntos[,get(cols_qm_dirs[j])],leyenda = c("Mean",paste0('uv_wind',i),cols_qm[j],cols_qm_dirs[j]),main = paste0('Punto ',i))
-  } 
+#Taylor
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(col_era)],add = F,main = paste0('Punto ',i,' - calibrado con dirs'),col = 'red')
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(paste0('qm',i,'_dirs'))],add = T,col = 'green')
 }
 
+#Calibrado sin dirs vs por dirs (1 vs 2)----
+
+#cols_qm_dirs=grep('qm\\d+_dirs',colnames(datos_juntos),value=T)
+#cols_qm=grep('qm\\d',colnames(datos_juntos),value=T) %>% .[!(. %in% cols_qm_dirs)]
+
+#Graficar punto por punto
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(col_era)],datos_juntos[,get(paste0('qm',i))],datos_juntos[,get(paste0('qm',i,'_dirs'))],leyenda = c("Mean",paste0('uv_wind',i),paste0('qm',i),paste0('qm',i,'_dirs')),main = paste0('Punto ',i))
+} 
+
+
 #Medidas
-for (i in (cols_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist)) {
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
   print(paste0('Punto ',i,' calibrado SIN DIRS vs anemo'))
   print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('qm',i))]))
   print(paste0('Punto ',i,' calibrado CON DIRS vs anemo'))
@@ -174,89 +200,30 @@ for (i in (cols_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist)) {
   print('----------------------')
 }
 
-#Preparar cross_validation ()----
+
+#Taylor
+for (col_era in cols_era) {
+  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(col_era)],add = F,main = paste0('Punto ',i,' - calibrado sin dirs (berde) vs con dirs (azul) '),col = 'red')
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(paste0('qm',i))],add = T,col = 'green')
+  taylor.diagram(ref = datos_juntos$Mean,model = datos_juntos[,get(paste0('qm',i,'_dirs'))],add = T,col = 'blue')
+}
 
 
-nPartes=  #Numero de partes (de 'cv folds') en las que se separan los datos para la cross-validation
+#Preparar cross_validation (solo dos partes, peor entrenamiento posible sin liarla mucho con la cross validation)----
+
+nPartes=2  #"Leave-one-out", se entrena el modelo para cada dato calibrado
 
 (range(datos_juntos$Date)[2]-range(datos_juntos$Date)[1])/nPartes #Cuanto tiempo tendra cada parte?
 for (i in 1:nPartes) {
   datos_juntos[round((i-1)*nrow(datos_juntos)/nPartes):round(i*nrow(datos_juntos)/nPartes),Parte := i]
 }
 
+#Pruebas potencia----
+library(MASS)
+fit = fitdistr(datos_juntos[Mean > 0,Mean],"weibull") #No acepta ceros
+k<-coef(fit)['shape']
+c<-coef(fit)['scale']
 
-#3. Como el 1 pero invirtiendo los periodos de entrenamiento y validación----
-
-(range(datos_juntos$Date)[2]-range(datos_juntos$Date)[1])/nPartes #Con cuanto tiempo se va a entrenar?
-
-#Crear funciones de transferencia (ftrans). #La de cada parte se entrena con las mediciones que NO son de la parte (Parte != i)
-lista_ftrans=list() #Lista de listas.
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
-  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
-  lista_ftrans[[i]]=list()
-  for (j in 1:nPartes) {
-    ftrans=fitQmapQUANT(datos_juntos[Parte == j,Mean],datos_juntos[Parte == j,get(col_era)])
-    #lista_ftrans=append(lista_ftrans,values = list(ftrans_i))  esto solo sirve si las columnas uv_wind estan ordenadas
-    lista_ftrans[[i]]=append(lista_ftrans[[i]],values = list(ftrans))
-  }
-  
-}
-
-#Calibrar.
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
-  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
-  for (j in 1:nPartes) {
-    datos_juntos[Parte != j,paste0('inv_qm',i) := doQmapQUANT(datos_juntos[Parte != j,get(col_era)],lista_ftrans[[i]][[j]])]
-  }
-}
-
-#Graficar punto por punto
-cols_inv_qm_dirs=grep('inv_qm\\d+_dirs',colnames(datos_juntos),value=T)
-cols_inv_qm=grep('inv_qm\\d',colnames(datos_juntos),value=T) %>% .[!(. %in% cols_inv_qm_dirs)]
-for (cols_inv_qm[j] in cols_inv_qm) {
-  i=regmatches(cols_inv_qm[j], regexpr("\\d+", cols_inv_qm[j]))  #numero del punto de era
-  plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(paste0('uv_wind',i))],datos_juntos[,get(cols_inv_qm[j])],leyenda = c("Mean",paste0("ERA",i),cols_inv_qm[j]),main = paste0('Punto ',i))
-}
-
-#Graficar todos los puntos, pero solo el calibrado y el anemo (codigo hecho para 9 puntos)
-plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,
-                datos_juntos$inv_qm1,datos_juntos$inv_qm2,datos_juntos$inv_qm3,datos_juntos$inv_qm4,datos_juntos$inv_qm5,datos_juntos$inv_qm6,datos_juntos$inv_qm7,datos_juntos$inv_qm8,datos_juntos$inv_qm9,leyenda = c("Mean","inv_qm1","inv_qm2","inv_qm3","inv_qm4","inv_qm5","inv_qm6","inv_qm7","inv_qm8","inv_qm9"),col = 'black')
-
-#Medidas
-for (col_era in grep('uv_wind',colnames(datos_juntos),value=T)) {
-  i=regmatches(col_era, regexpr("\\d+", col_era))  #numero del punto de era
-  print(paste0('Punto ',i,' a pelo vs anemo'))
-  print(accuracy(datos_juntos$Mean,datos_juntos[,get(col_era)]))
-  print(paste0('Punto ',i,' calibrado vs anemo'))
-  print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('inv_qm',i))]))
-  print('----------------------')
-}
-
-
-
-#Mucho entrenamiento vs poco entrenamiento (1 vs 3)----
-
-cols_inv_qm=grep('inv_qm\\d+',colnames(datos_juntos),value=T)
-cols_qm_dirs=grep('qm\\d+_dirs',colnames(datos_juntos),value=T)
-cols_qm=grep('qm\\d',colnames(datos_juntos),value=T) %>% .[!(. %in% c(cols_inv_qm,cols_qm_dirs))]
-
-#Graficar punto por punto
-#Si los puntos que de era que tienen columna 'qm\\d+' no son los mismos que tienen 'qm_\\d+_dirs'...
-if(any((cols_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist %>% order)!=(cols_inv_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist %>% order))) {
-  print('ERROR. Parece que no has calibrado los mismos puntos sin dirs que con dirs. Estas seguro de que has hecho las dos calibraciones?')
-}else{
-  for (j in 1:length(cols_qm)) {
-    i=regmatches(cols_qm[j], regexpr("\\d+", cols_qm[j]))  #numero del punto de era
-    plot_n_graficos(x=datos_juntos$Date,n=10,datos_juntos$Mean,datos_juntos[,get(paste0('uv_wind',i))],datos_juntos[,get(cols_qm[j])],datos_juntos[,get(cols_inv_qm[j])],leyenda = c("Mean",paste0('uv_wind',i),cols_qm[j],cols_inv_qm[j]),main = paste0('Punto ',i))
-  } 
-}
-
-#Medidas
-for (i in (cols_qm %>% regmatches(.,gregexpr("\\d+", .)) %>% unlist)) {
-  print(paste0('Punto ',i,' calibrado mucho tiempo vs anemo'))
-  print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('qm',i))]))
-  print(paste0('Punto ',i,' calibrado poco tiempo vs anemo'))
-  print(accuracy(datos_juntos$Mean,datos_juntos[,get(paste0('inv_qm',i))]))
-  print('----------------------')
-}
-
+summary(datos_juntos$Mean)
+histo=datos_juntos$Mean %>% hist(.,seq(0, max(.), by=0.1))
